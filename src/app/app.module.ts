f7db5c1033a5fb0e7e8912deaf897b4258ea5227
chrisCash2020 <chrisolo2001@gmail.com>
2023-12-21 23:50:17 -0600

    removed community component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { LandingNavComponent } from './components/landing-nav/landing-nav.component';
 import { LandingHeroComponent } from './components/landing-hero/landing-hero.component';
 import { LandingComponent } from './pages/landing/landing.component';
-import { LandingCommunityComponent } from './components/landing-community/landing-community.component';
 import { LandingMarketingComponent } from './components/landing-marketing/landing-marketing.component';
 import { LandingFooterComponent } from './components/landing-footer/landing-footer.component';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
@@ -23,7 +22,6 @@ import { RegistrationOnboardingScreenComponent } from './components/registration
     LandingNavComponent,
     LandingHeroComponent,
     LandingComponent,
-    LandingCommunityComponent,
     LandingMarketingComponent,
     LandingFooterComponent,
     RegistrationFormComponent,
